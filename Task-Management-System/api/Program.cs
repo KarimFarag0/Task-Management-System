0d866c0523a49818d611f4ae739277584a8a040d
Karim_F <karim.michelf@gmail.com>
2025-03-21 12:27:33 -0400

    Changes

--- Task-Management-System/api/Program.cs
+++ Task-Management-System/api/Program.cs
@@ -1,76 +1,112 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+using API.Data;
+
+// Builder
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); // string
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin() // Permite todas as origens (em desenvolvimento)
+                   .AllowAnyMethod() // Permite todos os métodos HTTP
+                   .AllowAnyHeader(); // Permite todos os cabeçalhos
+        });
+});
+//
+
+
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(
+    options => {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        // options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+)
+.AddJwtBearer(
+    options => {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    }
+);
+
+builder.Services.AddAuthorization(
+    options => 
+    {
+        options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+        options.AddPolicy("RequireUserRole", policy => policy.RequireRole("User"));
+        options.AddPolicy("RequireDeveloperRole", policy => policy.RequireRole("Developer"));
+    }
+);
+
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); // Easy Way to do the API documentation 
 
+// stage 2
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// CORS only in development mode
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("AllowAllOrigins");
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    if(!(await roleManager.RoleExistsAsync("User"))){
+        await roleManager.CreateAsync(new IdentityRole("User"));
+    }
+
+    if(!(await roleManager.RoleExistsAsync("Admin"))){
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+
+    if(!(await roleManager.RoleExistsAsync("Developer"))){
+        await roleManager.CreateAsync(new IdentityRole("Developer"));
+    }
+}
+
+
+if(app.Environment.IsDevelopment()){
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// if not in Development or is in Production
+if(!app.Environment.IsDevelopment()){
+    app.UseHttpsRedirection();
+}
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-
-
-app.MapGet("/", () =>
-{
-   
-    return "API is Working";
-})
-.WithName("GetHome");
-
-//Implement a new endpoitn "/{price}/{tax}/"
-//return JSON
-
-//{
-        //price: 0.00
-        //tax: 0%
-        //final: price + tax
-//}
-
-app.MapGet("{price}/{tax}", (decimal price, decimal tax) => 
-{
-    var finalPrice = price + (price * tax / 100);
-    return Results.Json(new 
-    {
-        price = price,
-        tax = tax,
-        final = finalPrice
-    });
-})
-.WithName("GetPriceWithTax");
-
-
-// Weather Forecase "http//localhost<PORT>/weatherforecast"
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
